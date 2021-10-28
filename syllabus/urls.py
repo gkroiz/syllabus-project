e15a69f388e233ae5f6f50f8cf4b13406644c453
bailor1 <bailor1@umbc.edu>
2021-10-28 00:19:04 -0400

    create seperate app for homepage

--- syllabus/urls.py
+++ syllabus/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('faculty_profile.urls'))
+    path('', include('homepage.urls')),
+    path('profile/', include('faculty_profile.urls'))
 ]
