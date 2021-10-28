d6dde7a114f17f8b72de678f913a8eb8a6394ada
bailor1 <bailor1@umbc.edu>
2021-10-28 00:20:04 -0400

    create webapp for homepage with welcome view

---
+++ homepage/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # home page
+    path('', views.index, name='index'),
+
+    # routes from home page
+    path('login/', views.index, name='login'),
+    path('search/', views.index, name='search'),
+]
