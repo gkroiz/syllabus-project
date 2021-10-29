5a7dad6ebc5a00238f1af0538d34480ac744c4f0
bailor1 <bailor1@umbc.edu>
2021-10-28 20:24:26 -0400

    add basic placeholder view for search and login, which will later be connected to their corresponding apps

--- homepage/views.py
+++ homepage/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello welcome to the UMBC Syllabus Synthesizer")
+    return render(request, 'homepage/index.html', context={})
+
+
+def login(request):
+    return HttpResponse("View will be connected to the login app later")
+
+
+def search(request):
+    return HttpResponse("View will be connected to the search app later")
