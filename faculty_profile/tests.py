a60733ad55bcaea3d69b197e2f39f93bcb9ef1af
bailor1 <bailor1@umbc.edu>
2021-10-29 01:55:33 -0400

    test profile model/database table

--- faculty_profile/tests.py
+++ faculty_profile/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Profile
+
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        Profile.objects.create(ID="123VT45", name="John Benson", email="jbenson1@umbc.edu", location="ILSB 420",
+                               phone="+1 (123) 456-7891", hours="10-11 AM M/W")
+
+    def test_animals_can_speak(self):
+        benson = Profile.objects.get(ID="123VT45")
+        self.assertEqual(benson.ID, "123VT45")
+        self.assertEqual(benson.name, "John Benson")
+        self.assertEqual(benson.email, "jbenson1@umbc.edu")
+        self.assertEqual(benson.location, "ILSB 420")
+        self.assertEqual(benson.phone, "+1 (123) 456-7891")
+        self.assertEqual(benson.hours, "10-11 AM M/W")
