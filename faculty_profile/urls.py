760108d72ebdd5025a7dbcbddfaaca8686987306
bailor1 <bailor1@umbc.edu>
2021-10-29 13:23:54 -0400

    create add() view and url to let edit/ form POST interact with database

--- faculty_profile/urls.py
+++ faculty_profile/urls.py
@@ -6,7 +6,8 @@ app_name = 'faculty_profile'
 urlpatterns = [
     # profile index (placeholder, should be deleted in the future)
     path('', views.index, name='index'),
-    path('edit/', views.edit, name='edit')
+    path('edit/', views.edit, name='edit'),
+    path('add/', views.add, name='add')
 
     # will the login app and corresponding session database provide a faculty's profile_id?
     # path('<int:profile_id>', views.index, name='index'),
--- faculty_profile/views.py
+++ faculty_profile/views.py
@@ -1,10 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from faculty_profile.models import Profile
 
 
 def index(request):
-    return render(request, 'faculty_profile/index.html', context={})
+    profiles = Profile.objects.filter()
+    return render(request, 'faculty_profile/index.html', context={'profiles': profiles})
 
 
 def edit(request):
     return render(request, 'faculty_profile/edit.html', context={})
+
+
+def add(request):
+    # if ID from form POST is the same as an ID that is already in the database, the existing entry will be modified
+    # if ID is new, a new entry will be created
+    profile_id = request.POST['ID']
+    name = request.POST['name']
+    email = request.POST['email']
+    location = request.POST['location']
+    phone = request.POST['phone']
+    hours = request.POST['hours']
+    profile_obj = Profile(ID=profile_id, name=name, email=email, location=location, phone=phone, hours=hours)
+    profile_obj.save()
+    return HttpResponseRedirect(reverse('faculty_profile:index'))
