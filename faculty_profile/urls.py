7ff211669fa16b1f76b9db16abae79609f842b69
bailor1 <bailor1@umbc.edu>
2021-10-28 20:53:45 -0400

    namespace the urls and correct views.path

--- faculty_profile/urls.py
+++ faculty_profile/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'faculty_profile'
 urlpatterns = [
     # profile page(s)
     path('', views.index, name='index'),
-    path('<int:profile_id>/', views.index, name='profile'),
-    path('<int:profile_id>/edit/', views.index, name='profile_edit')
+    path('<int:profile_id>/', views.profile, name='profile'),
+    path('<int:profile_id>/edit/', views.edit, name='edit')
 ]
