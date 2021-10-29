ef322287d1fa6ecd7e8f364f64387df68f864b10
bailor1 <bailor1@umbc.edu>
2021-10-29 01:55:17 -0400

    remove profile id from url path until integration with login session db

--- faculty_profile/urls.py
+++ faculty_profile/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 app_name = 'faculty_profile'
 urlpatterns = [
-    # profile page(s)
+    # profile index (placeholder, should be deleted in the future)
     path('', views.index, name='index'),
-    path('<int:profile_id>/', views.profile, name='profile'),
-    path('<int:profile_id>/edit/', views.edit, name='edit')
+    path('edit/', views.edit, name='edit')
+
+    # will the login app and corresponding session database provide a faculty's profile_id?
+    # path('<int:profile_id>', views.index, name='index'),
+    # path('<int:profile_id>/edit/', views.edit, name='edit')
 ]
