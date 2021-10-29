c7f87bc70f16f5fca3c6a5ed364d5db373aa318b
bailor1 <bailor1@umbc.edu>
2021-10-29 01:53:51 -0400

    add location field to profile model

--- faculty_profile/models.py
+++ faculty_profile/models.py
@@ -7,7 +7,8 @@ from django.db import models
 # attributes: ID, name, email, office phone, office location, office hours
 class Profile(models.Model):
     ID = models.CharField(max_length=200, primary_key=True)
-    name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    office_phone = models.CharField(max_length=20)
-    office_hours = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default='None')
+    email = models.CharField(max_length=200, default='None')
+    location = models.CharField(max_length=200, default='None')
+    phone = models.CharField(max_length=20, default='+1 (000) 000-0000')
+    hours = models.CharField(max_length=200, default='None')
