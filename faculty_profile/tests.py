be356359af49b5f32580d36156f7028c3200d6da
bailor1 <bailor1@umbc.edu>
2021-11-02 03:00:49 -0400

    test that the views exist at the proper URLs so that response code is 200

--- faculty_profile/tests.py
+++ faculty_profile/tests.py
@@ -2,8 +2,9 @@ from django.test import TestCase
 from .models import Profile
 
 
-# Create your tests here.
-class ProfileTestCase(TestCase):
+# Test for proper Profile database functionality
+class ProfileTests(TestCase):
+
     def setUp(self):
         Profile.objects.create(ID="123VT45", name="John Benson", email="jbenson1@umbc.edu", location="ILSB 420",
                                phone="+1 (123) 456-7891", hours="10-11 AM M/W")
@@ -16,3 +17,15 @@ class ProfileTestCase(TestCase):
         self.assertEqual(benson.location, "ILSB 420")
         self.assertEqual(benson.phone, "+1 (123) 456-7891")
         self.assertEqual(benson.hours, "10-11 AM M/W")
+
+
+# Test for URL routes and Views
+class ViewTests(TestCase):
+
+    def test_homepage_view(self):
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_view(self):
+        response = self.client.get('/profile/edit/')
+        self.assertEqual(response.status_code, 200)
