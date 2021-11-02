c03539b112e7d4e8181a79b52dd72e72c04a8999
bailor1 <bailor1@umbc.edu>
2021-11-02 02:48:45 -0400

    test that the proper views exist at the proper URLs so that response code is 200

--- homepage/tests.py
+++ homepage/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+# Test for URL routes and Views
+class ViewTests(TestCase):
+
+    def test_homepage_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_view(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_view(self):
+        response = self.client.get('/search/')
+        self.assertEqual(response.status_code, 200)
