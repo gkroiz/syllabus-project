04d072cb8eb9a1ff0248290328bccb0d83151546
bailor1 <78231932+bailor1@users.noreply.github.com>
2021-11-04 21:20:45 -0400

    Update admin.py
    did not end up making any changes to the file, so reset back to default

--- syllabus_form/admin.py
+++ syllabus_form/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-# from .models import Course 
+
 # Register your models here.
-
-
-# admin.site.register(Snippet)
-
-# class 
-
-# class CourseAdmin(admin.TabularInline):
-    # model = Course
