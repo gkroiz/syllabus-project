3ccf0cfd0fd0bd26e9f6b323b3ce927d5f4e37dc
bailor1 <78231932+bailor1@users.noreply.github.com>
2021-11-04 21:13:36 -0400

    Update settings.py
    unused library reference/call

--- syllabus/settings.py
+++ syllabus/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'syllabus.urls'
