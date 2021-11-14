6b2ad0bbb81e100632600f8b3965834469fa8127
Deep Mistry <dmistry1@umbc.edu>
2021-11-13 19:32:11 -0500

    configured so welcome page goes to login page

--- login/urls.py
+++ login/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url
 from django.urls import path
 
 from . import views
-
+app_name = 'login'
 urlpatterns = [
 
-    path('', views.index, name='index'),
+
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_view, name='logout_view'),
-    path('login/', views.login_view, name='login')
+    path('', views.login_view, name='login')
 
 ]
