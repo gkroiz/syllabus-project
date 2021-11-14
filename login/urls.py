bc4b9a8e787149458e440f9c9d0585c0086e7e37
Deep Mistry <dmistry1@umbc.edu>
2021-11-13 19:23:24 -0500

    Changed the way users were getting stored

--- login/urls.py
+++ login/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 
 from . import views
 
-app_name = 'login'
 urlpatterns = [
+
     path('', views.index, name='index'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout_view, name='logout_view'),
+    path('login/', views.login_view, name='login')
+
 ]
