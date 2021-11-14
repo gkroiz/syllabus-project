4c3982098d3e7b4b41551f238754d2aa2f330785
bailor1 <bailor1@umbc.edu>
2021-11-13 19:14:43 -0500

    namespace login app urls

--- login/urls.py
+++ login/urls.py
@@ -3,9 +3,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'login'
 urlpatterns = [
-
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup')
-
 ]
