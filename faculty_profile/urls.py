61883ac755ba1567bfc7e817e57ac19575c8c7dc
bailor1 <bailor1@umbc.edu>
2021-11-17 21:13:37 -0500

    all profile URLs attached to user_id

--- faculty_profile/urls.py
+++ faculty_profile/urls.py
@@ -6,9 +6,9 @@ app_name = 'faculty_profile'
 urlpatterns = [
     # profile index (placeholder, should be deleted in the future)
     path('<str:user_id>/', views.index, name='index'),
-    path('edit/', views.edit, name='edit'),
-    path('add/', views.add, name='add'),
-    path('syllabus/', views.syllabus, name='syllabus')
+    path('<str:user_id>/edit/', views.edit, name='edit'),
+    path('<str:user_id>/add/', views.add, name='add'),
+    path('<str:user_id>/syllabus/', views.syllabus, name='syllabus')
 
     # will the login app and corresponding session database provide a faculty's profile_id?
     # path('<int:profile_id>', views.index, name='index'),
