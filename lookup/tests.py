8022a4b3877effe3d016d8dc649427c09982f608
ksamuel890 <koby@kobysamuel.com>
2021-12-05 17:45:37 -0500

    added feature for searching syllabi based on synthesizew Syllabus object and associated tests.py

--- lookup/tests.py
+++ lookup/tests.py
@@ -1,3 +1,185 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
 from django.test import TestCase
+from synthesizew.models import Syllabus
+from django.test import LiveServerTestCase
+import time
 
-# Create your tests here.
+
+# tests search subdomain fully accessible
+class ViewTests(TestCase):
+    def test_lookup_view(self):
+        response = self.client.get('/search/')
+        self.assertEqual(response.status_code, 200)
+
+
+class LookupCourseTests(LiveServerTestCase):
+    # sets up Syllabus objects so search can occur
+    # @classmethod
+    # def setUpClass(cls):
+    #     super().setUpClass()
+    #     print("test A")
+    #     cls.syllabus_1 =
+    #     cls.syllabus_1.save()
+    #     print("test B")
+
+    # runs search for course title and gets results based on inputted db entry
+    def test_search_for_course_title(self):
+        Syllabus.objects.create(syllabus_file='syllabus.pdf',
+                                title='TUFF 101',
+                                instructor_name='Dr. Hrabowski',
+                                instructor_phone='(410) 455-1000',
+                                instructor_email='noreply@umbc.edu',
+                                ta_name='Philip Rous',
+                                ta_email='UMBCHelp@rt.umbc.edu',
+                                course_site='https://blackboard.umbc.edu',
+                                office_hours='2-3am',
+                                course_time='9am-5pm',
+                                course_description='A course that didn\'t have one!',
+                                course_objectives='Just try your hardest!',
+                                prereqs='Admission to UMBC, a willingness to succeed, and a passion for more',
+                                textbook='Dune by Frank Herbert',
+                                instruct_methods='IRL talking',
+                                attendance_rule='Stick around!',
+                                class_preparation='Listen, learn, grow',
+                                course_requirements='See class preparation steps',
+                                grade_breakdown='If you try, you get 100%',
+                                quizzes='Also called \'bad news\'',
+                                exams='Those really tough days and situations',
+                                prog_assignments='Arguments and confrontations',
+                                participation='100% of your grade is being there',
+                                hands_on='See participation grade',
+                                assignments='Be the best you you can be',
+                                homework='Life has no homework (wish school was the same,',
+                                late_policy='Whenever it suits you',
+                                makeup_policy='There are no second chances').save()
+        selenium = webdriver.Chrome(executable_path='chromedriver')
+        selenium.get('%s%s' % (self.live_server_url, '/search/'))
+
+        selenium.implicitly_wait(1000)
+
+        query_type_select = Select(selenium.find_element_by_id("dropdown"))
+        query_type_select.select_by_value("course_title")
+
+        course_title_input = selenium.find_element_by_id("search_bar")
+        course_title_input.send_keys("TUFF 101")
+
+        submit_input = selenium.find_element_by_id("submit_click")
+        submit_input.click()
+
+        time.sleep(2)
+
+        try:
+            selenium.find_element_by_id("results_header")
+        except NoSuchElementException:
+            self.assertTrue(0)
+
+        selenium.quit()
+
+    # runs search for course instructor and gets results based on inputted db entry
+    def test_search_for_instructor(self):
+        Syllabus.objects.create(syllabus_file='syllabus.pdf',
+                                title='TUFF 101',
+                                instructor_name='Dr. Hrabowski',
+                                instructor_phone='(410) 455-1000',
+                                instructor_email='noreply@umbc.edu',
+                                ta_name='Philip Rous',
+                                ta_email='UMBCHelp@rt.umbc.edu',
+                                course_site='https://blackboard.umbc.edu',
+                                office_hours='2-3am',
+                                course_time='9am-5pm',
+                                course_description='A course that didn\'t have one!',
+                                course_objectives='Just try your hardest!',
+                                prereqs='Admission to UMBC, a willingness to succeed, and a passion for more',
+                                textbook='Dune by Frank Herbert',
+                                instruct_methods='IRL talking',
+                                attendance_rule='Stick around!',
+                                class_preparation='Listen, learn, grow',
+                                course_requirements='See class preparation steps',
+                                grade_breakdown='If you try, you get 100%',
+                                quizzes='Also called \'bad news\'',
+                                exams='Those really tough days and situations',
+                                prog_assignments='Arguments and confrontations',
+                                participation='100% of your grade is being there',
+                                hands_on='See participation grade',
+                                assignments='Be the best you you can be',
+                                homework='Life has no homework (wish school was the same,',
+                                late_policy='Whenever it suits you',
+                                makeup_policy='There are no second chances').save()
+        print(Syllabus.objects.filter(title__contains="TUFF"))
+        selenium = webdriver.Chrome(executable_path='chromedriver')
+        selenium.get('%s%s' % (self.live_server_url, '/search/'))
+
+        selenium.implicitly_wait(1000)
+
+        query_type_select = Select(selenium.find_element_by_id("dropdown"))
+        query_type_select.select_by_value("instructor_name")
+
+        course_title_input = selenium.find_element_by_id("search_bar")
+        course_title_input.send_keys("Hrabowski")
+
+        submit_input = selenium.find_element_by_id("submit_click")
+        submit_input.click()
+
+        time.sleep(2)
+
+        try:
+            selenium.find_element_by_id("results_header")
+        except NoSuchElementException:
+            self.assertTrue(0)
+
+        selenium.quit()
+
+    # runs search for course time and gets results based on inputted db entry
+    def test_search_for_course_time(self):
+        Syllabus.objects.create(syllabus_file='syllabus.pdf',
+                                title='TUFF 101',
+                                instructor_name='Dr. Hrabowski',
+                                instructor_phone='(410) 455-1000',
+                                instructor_email='noreply@umbc.edu',
+                                ta_name='Philip Rous',
+                                ta_email='UMBCHelp@rt.umbc.edu',
+                                course_site='https://blackboard.umbc.edu',
+                                office_hours='2-3am',
+                                course_time='9am-5pm',
+                                course_description='A course that didn\'t have one!',
+                                course_objectives='Just try your hardest!',
+                                prereqs='Admission to UMBC, a willingness to succeed, and a passion for more',
+                                textbook='Dune by Frank Herbert',
+                                instruct_methods='IRL talking',
+                                attendance_rule='Stick around!',
+                                class_preparation='Listen, learn, grow',
+                                course_requirements='See class preparation steps',
+                                grade_breakdown='If you try, you get 100%',
+                                quizzes='Also called \'bad news\'',
+                                exams='Those really tough days and situations',
+                                prog_assignments='Arguments and confrontations',
+                                participation='100% of your grade is being there',
+                                hands_on='See participation grade',
+                                assignments='Be the best you you can be',
+                                homework='Life has no homework (wish school was the same,',
+                                late_policy='Whenever it suits you',
+                                makeup_policy='There are no second chances').save()
+        selenium = webdriver.Chrome(executable_path='chromedriver')
+        selenium.get('%s%s' % (self.live_server_url, '/search/'))
+
+        selenium.implicitly_wait(1000)
+
+        query_type_select = Select(selenium.find_element_by_id("dropdown"))
+        query_type_select.select_by_value("course_time")
+
+        course_title_input = selenium.find_element_by_id("search_bar")
+        course_title_input.send_keys("5pm")
+
+        submit_input = selenium.find_element_by_id("submit_click")
+        submit_input.click()
+
+        time.sleep(2)
+
+        try:
+            selenium.find_element_by_id("results_header")
+        except NoSuchElementException:
+            self.assertTrue(0)
+
+        selenium.quit()
--- lookup/views.py
+++ lookup/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 from synthesizew.models import Syllabus
 
 
@@ -10,8 +8,17 @@ def index(request):
 
 
 def results(request):
-    course_title = request.GET.get('title_search')
-    search_results = Syllabus.objects.filter(title__contains=course_title)
+    search_by = request.GET.get('dropdown')
+    search_results = {}
+    if search_by == 'course_title':
+        course_title = request.GET.get('search_bar')
+        search_results = Syllabus.objects.filter(title__contains=course_title)
+    elif search_by == 'instructor_name':
+        instructor_name = request.GET.get('search_bar')
+        search_results = Syllabus.objects.filter(instructor_name__contains=instructor_name)
+    elif search_by == 'course_time':
+        course_time = request.GET.get('search_bar')
+        search_results = Syllabus.objects.filter(course_time__contains=course_time)
     context = {
         "search_results": search_results
     }
