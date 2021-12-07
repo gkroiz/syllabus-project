034758d5a26843d9406c5d73bec1ecf6a52a59d1
bailor1 <bailor1@umbc.edu>
2021-12-06 19:00:31 -0500

    add tests for new database and form modifications, remove old view/url tests

--- faculty_profile/tests.py
+++ faculty_profile/tests.py
@@ -1,47 +1,51 @@
 from django.test import TestCase
 
-from .models import Profile
+from .models import Profile, OfficeHours
 from .forms import EditProfileForm
-
-
-# Test for URL routes and Views
-class ViewTests(TestCase):
-
-    def test_homepage_view(self):
-        response = self.client.get('/profile/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_edit_view(self):
-        response = self.client.get('/profile/edit/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_syllabus_view(self):
-        response = self.client.get('/profile/syllabus/')
-        self.assertEqual(response.status_code, 200)
 
 
 # Test for proper Profile database functionality
 class ProfileTests(TestCase):
 
     def setUp(self):
-        Profile.objects.create(ID="123VT45", location="ILSB 420", phone="+1 (123) 456-7891", hours="10-11 AM M/W")
+        Profile.objects.create(faculty_id='123VT45', location='ILSB 420', phone='+1 (123) 456-7891')
 
     def test_database_object_was_created(self):
-        benson = Profile.objects.get(ID="123VT45")
-        self.assertEqual(benson.ID, "123VT45")
-        self.assertEqual(benson.location, "ILSB 420")
-        self.assertEqual(benson.phone, "+1 (123) 456-7891")
-        self.assertEqual(benson.hours, "10-11 AM M/W")
+        benson = Profile.objects.get(faculty_id='123VT45')
+        self.assertEqual(benson.faculty_id, '123VT45')
+        self.assertEqual(benson.location, 'ILSB 420')
+        self.assertEqual(benson.phone, '+1 (123) 456-7891')
+
+
+# Test for proper Profile database functionality
+class OfficeHoursTests(TestCase):
+
+    def setUp(self):
+        profile = Profile.objects.create(faculty_id='123VT45', location='ILSB 420', phone='+1 (123) 456-7891')
+        OfficeHours.objects.create(faculty=profile, date_time='MWF 1-2PM')
+
+    def test_database_object_was_created(self):
+        benson = Profile.objects.get(faculty_id='123VT45')
+        benson_oh = OfficeHours.objects.get(faculty='123VT45')
+        self.assertEqual(benson.faculty_id, benson_oh.faculty.faculty_id)
+        self.assertEqual(benson_oh.date_time, 'MWF 1-2PM')
 
 
 # Test for proper Edit form functionality
-class EditTests(TestCase):
-    def test_forms(self):
-        form_data = {'ID': '123UMBC', 'location': 'ILSB 106b', 'phone': '+1 (444) 555-3322', 'hours':'MWF 1-5PM'}
+class FormTests(TestCase):
+    def test_good_form(self):
+        form_data = {'faculty_id': '123UMBC', 'location': 'ILSB 106b', 'phone': '+1 (444) 555-3322',
+                     'extra_field_count': 0}
         form = EditProfileForm(data=form_data)
         self.assertTrue(form.is_valid())
 
-    def test_bad_forms(self):
-        form_data = {'ID': '1', 'location': 'ITE', 'phone': '+1 (444) 555-33221111111111111111111', 'hours':'MWF 1-5PM'}
+    def test_bad_phone(self):
+        form_data = {'faculty_id': '1', 'location': 'ITE', 'phone': '+1 (444) 555-33221111111111111111111',
+                     'extra_field_count': 0}
+        form = EditProfileForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_missing_field(self):
+        form_data = {'faculty_id': '1', 'location': 'ITE', 'phone': '+1 (444) 555-3322'}
         form = EditProfileForm(data=form_data)
         self.assertFalse(form.is_valid())
